8c425a83a08eaa60774b302a7ffa2c0b1ad3d13f
Sunil Jacob <sunil1405@gmail.com>
2023-11-22 15:42:53 +0530

    Add method

--- struct.go
+++ struct.go
@@ -5,14 +5,23 @@ import (
 )
 
 func main() {
-	poodle := Dog{"Poodle", 10}
+	poodle := Dog{"Poodle", 10, "Woof!!"}
 	fmt.Println(poodle)
 	fmt.Printf("%+v\n", poodle)
 	fmt.Printf("Breed: %v\nWeight: %v\n", poodle.Breed, poodle.Weight)
+
+	poodle.Speak()
+	poodle.Sound = "Arrf!!"
+	poodle.Speak()
 }
 
 // Dog is a struct
 type Dog struct {
 	Breed  string
 	Weight int
+	Sound  string
+}
+
+func (d Dog) Speak() {
+	fmt.Println(d.Sound)
 }
