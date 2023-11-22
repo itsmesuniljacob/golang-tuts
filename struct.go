e4b33949521234c447f0b0dbe4c878ae767a153c
Sunil Jacob <sunil1405@gmail.com>
2023-11-22 15:34:38 +0530

    add struct

---
+++ struct.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	poodle := Dog{"Poodle", 10}
+	fmt.Println(poodle)
+	fmt.Printf("%+v\n", poodle)
+	fmt.Printf("Breed: %v\nWeight: %v\n", poodle.Breed, poodle.Weight)
+}
+
+// Dog is a struct
+type Dog struct {
+	Breed  string
+	Weight int
+}
