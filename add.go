bc8d85f7675142badf6c93fef56ba4381801712d
Sunil Jacob <sunil1405@gmail.com>
2023-11-22 15:32:10 +0530

    Update add.go

--- add.go
+++ add.go
@@ -3,9 +3,15 @@ package main
 import "fmt"
 
 func main() {
+
+	sum := add(1, 2)
 	multiSum, multiCount := addAllValues(4, 6, 7)
 	fmt.Println("The total is: ", multiSum)
 	fmt.Println("The total count of values: ", multiCount)
+}
+
+func add(value1, value2 int) int {
+
 }
 
 func addAllValues(values ...int) (int, int) {
